382ae52891fa2831a58e2284e5924d2926f92ed9
Igor Charuyskiy <haos77792@list.ru>
2023-03-30 21:51:13 +0300

    practies

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,36 @@
 import UnoCSS from "unocss/vite";
 import presetUno from "@unocss/preset-uno";
 import presetIcons from "@unocss/preset-icons";
+import presetWebFonts from "@unocss/preset-web-fonts";
+
 export default {
   plugins: [
     UnoCSS({
-      presets: [presetUno(), presetIcons()],
+      presets: [
+        presetUno(),
+        presetIcons(),
+        presetWebFonts({
+          provider: "google", // default provider
+          fonts: {
+            // these will extend the default theme
+            sans: "Roboto",
+            mono: ["Fira Code", "Fira Mono:400,700"],
+            // custom ones
+            lobster: "Lobster",
+            lato: [
+              {
+                name: "Noto sans",
+                weights: ["400", "700"],
+                italic: false,
+              },
+              {
+                name: "sans-serif",
+                provider: "none",
+              },
+            ],
+          },
+        }),
+      ],
       rules: [
         // your custom rules
       ],
