bf41f8d5801748b8ed34ffe4ff3030350c038929
Igor Charuyskiy <haos77792@list.ru>
2023-03-30 20:04:27 +0300

    new commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// vite.config.ts
+
+import UnoCSS from "unocss/vite";
+import presetUno from "@unocss/preset-uno";
+import presetIcons from "@unocss/preset-icons";
+export default {
+  plugins: [
+    UnoCSS({
+      presets: [presetUno(), presetIcons()],
+      rules: [
+        // your custom rules
+      ],
+    }),
+  ],
+};
