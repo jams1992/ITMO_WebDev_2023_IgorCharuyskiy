997313aec3c1d1be387b3d9d332a3310b4c7cc8b
Igor Charuyskiy <haos77792@list.ru>
2023-04-13 16:03:13 +0300

    popup in process

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import "uno.css";
 import "@unocss/reset/tailwind.css";
+
+const domBtnCreateTask = document.getElementById("btnCreateTask");
+const domPopupCreateTask = document.getElementById("popupCreateTask");
+
+domBtnCreateTask.onclick = () => {
+  console.log("click");
+  domPopupCreateTask.classList.remove("hidden");
+
+  const domBtnCloseCreateTaskPopup = document.getElementById("btnCloseCreateTaskPopup");
+  domBtnCloseCreateTaskPopup.onclick = () => {
+    domPopupCreateTask.classList.add("hidden");
+    domBtnCloseCreateTaskPopup.onclick = null;
+  };
+};
