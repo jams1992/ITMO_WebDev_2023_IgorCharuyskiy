d554284c5ea5968aba2026da0bc29b807f3fb1c8
Igor Charuyskiy <haos77792@list.ru>
2023-04-25 13:31:08 +0300

    cypress delete task

--- cypress/e2e/taski.cy.js
+++ cypress/e2e/taski.cy.js
@@ -3,33 +3,15 @@ import DOM from '../../src/constants/dom';
 const SERVER_URL = 'http://localhost:5173/';
 
 describe('Test Todo Page', () => {
-  beforeEach(() => {
-    cy.intercept('/**/TaskPopup**').as('getTaskPopup');
-  });
-  it.only('user open main page and create task', () => {
-    cy.visit(SERVER_URL);
-    cy.url().should('include', SERVER_URL);
-
-    cy.get(`#${DOM.Popup.CONTAINER}`)
-      .should('exist')
-      .should('have.class', 'hidden');
-
-    cy.get(`#${DOM.Popup.CONTAINER}`)
-      .should('exist')
-      .should('have.class', 'hidden')
-      .find('.spinner')
-      .should('exist');
-
-    cy.get(`#${DOM.Button.CREATE_TASK}`)
+  const clickOnCreateTaskButton = () =>
+    cy
+      .get(`#${DOM.Button.CREATE_TASK}`)
       .should('exist')
       .should('contain.text', 'Create Task')
       .click();
 
-    cy.wait('@getTaskPopup');
-
+  const createTaskFromPopup = (todoTaskText) => {
     const popupTask = cy.get('[data-test-id="task-popup"]');
-
-    const todoTaskText = 'Welcome TASK';
 
     popupTask.should('exist').should('be.visible');
 
@@ -43,13 +25,72 @@ describe('Test Todo Page', () => {
       .should('exist')
       .should('contain.text', 'Create')
       .click();
+  };
 
-    cy.get('[data-test-id="tasks-column"]')
+  const getColumnChildren = () =>
+    cy.get('[data-test-id="tasks-column"]').should('exist').children();
+
+  const checkNumberOfTaskInColumnMatch = (numberOfTasks) => {
+    getColumnChildren().should('have.length', numberOfTasks + 1);
+  };
+
+  beforeEach(() => {
+    cy.visit(SERVER_URL);
+    cy.url().should('include', SERVER_URL);
+    cy.intercept('/**/TaskPopup**').as('getTaskPopup');
+  });
+  it('user open main page and create task', () => {
+    cy.get(`#${DOM.Popup.CONTAINER}`)
       .should('exist')
-      .children()
+      .should('have.class', 'hidden');
+
+    cy.get(`#${DOM.Popup.CONTAINER}`)
+      .should('exist')
+      .should('have.class', 'hidden')
+      .find('.spinner')
+      .should('exist');
+
+    clickOnCreateTaskButton();
+
+    cy.wait('@getTaskPopup');
+
+    const todoTaskText = 'Welcome TASK';
+    createTaskFromPopup(todoTaskText);
+
+    getColumnChildren()
       .should('have.length', 2)
       .first()
       .find('[data-id="templateTaskTitle"]')
       .should('contain.text', todoTaskText);
+  });
+
+  it.only('user create tasks and delete one', () => {
+    const tasks = ['Welcome TASK', 'Read books'];
+
+    tasks.forEach((text, index) => {
+      clickOnCreateTaskButton();
+      if (index === 0) cy.wait('@getTaskPopup');
+      createTaskFromPopup(text);
+    });
+    checkNumberOfTaskInColumnMatch(tasks.length);
+    getColumnChildren()
+      .first()
+      .find('[data-btn="btnDelete"]')
+      .should('exist')
+      .click();
+
+    const popupTask = cy.get('[data-test-id="task-popup"]');
+    popupTask
+      .find('[data-id="btnConfirm"]')
+      .should('exist')
+      .should('contain.text', 'Delete')
+      .click();
+
+    tasks.pop();
+
+    checkNumberOfTaskInColumnMatch(tasks.length);
+    tasks.forEach((text) => {
+      getColumnChildren().should('contain.text', text);
+    });
   });
 });
