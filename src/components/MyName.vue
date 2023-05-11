8a8d8ca774b9b397a52d3129d162d99a4edc763e
Igor Charuyskiy <haos77792@list.ru>
2023-05-11 18:20:22 +0300

    lesson 26 commit

---
+++ src/components/MyName.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Enter you name</h1>
+  <div>
+    {{ firstName }}<span style="color: red">{{ lastName }}</span>
+  </div>
+</template>
+<script> 
+export default {
+  name: 'MyName', 
+  // data: () => ({
+  //   firstName: "Igor",
+  //   lastName: "Charuyskiy",
+  // }),
+  data () {
+    return{
+      firstName: 'Igor',
+      lastName: 'Charuyskiy',
+    }
+
+  },
+  }
+</script>
+<style scoped></style>
