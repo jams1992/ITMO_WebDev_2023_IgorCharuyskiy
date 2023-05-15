e497416dae92f059e2a61d1e6359f3f38165db5e
Igor Charuyskiy <haos77792@list.ru>
2023-05-15 18:48:40 +0300

    final commit lesson 28

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,25 @@
   <AppHeader>
     Todo App
     <template #sub-header>
-      created by Igor
+      <span v-if="user">created by {{ user.name }}</span>
+      <span v-else>noname</span>
     </template>
   </AppHeader>
   <input
     ref="domInput"
     v-model="inputText"
-    @keyup.enter="canAddItemToTodoList && onInputKeyUp()"
+    @keyup.enter="canAddItemToTodoList && onInputEnterKeyUp()"
   >
   <div>
     List:
     <span v-if="todos.length">
-      {{ getTodoCount }}</span>
+      {{ getTodoCount }}
+    </span>
     <span v-else>empty</span>
     <TodoItem
       v-for="(item, index) in todos"
       :key="item"
-      :index="index+1"
+      :index="index + 1"
       :text="item"
       @delete="onDeleteTodo(index)"
     />
@@ -29,66 +31,62 @@
 import TodoItem from './components/TodoItem.vue';
 import AppHeader from './components/AppHeader.vue';
 
-const LOCAL_KEY_TODOS='todos';
-const LOCAL_KEY_INPUT_TEXT='input_text';
-let todoStopWath;
+const LOCAL_KEY_TODOS = 'todos';
+const LOCAL_KEY_INPUT_TEXT = 'input_text';
 
+let todoStopWatch;
 
 export default {
-  components:{TodoItem},
+  components: {AppHeader, TodoItem},
   data: () => ({
     inputText: null,
-    todos: []
+    todos: [],
+    user: { name: 'Igor' },
   }),
-  computed:{
-   
-   canAddItemToTodoList(){
-      return this.todoText?.length>0;
-   },
-   getTodoCount(){return this.todos.length>3?'many':this.todos.length;},
-  
-   todoText() {return this.inputText?.trim();},
-  
+  computed: {
+    canAddItemToTodoList() {
+      return this.todoText?.length > 0;
+    },
+    getTodoCount() { return this.todos.length > 3 ? 'many' : this.todos.length; },
+    todoText() { return this.inputText?.trim(); }
   },
-  created(){
-    const rawTodos=localStorage.getItem(LOCAL_KEY_TODOS);
-    console.log('>App->created:rawTodos=',rawTodos);
-    if(rawTodos){
-      this.todos=JSON.parse(rawTodos);
+  created() {
+    const rawTodos = localStorage.getItem(LOCAL_KEY_TODOS);
+    console.log('> App -> created: rawTodos =', rawTodos);
+    if (rawTodos) {
+      this.todos = JSON.parse(rawTodos);
     }
-    this.inputText=JSON.parse(localStorage.getItem(LOCAL_KEY_INPUT_TEXT)|| ''); 
+    this.inputText = JSON.parse(localStorage.getItem(LOCAL_KEY_INPUT_TEXT) || '""');
 
-    todoStopWath=this.$watch(()=>this.todos,(value)=>{
-      console.log('>App->watch:todos=',value);
-      localStorage.setItem(LOCAL_KEY_TODOS,JSON.stringify(value));
-    },{deep:true});
+    // fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    //     .then((response) => response.json())
+    //     .then((rawDataList) => this.todos = rawDataList.slice(0, 5).map((item) => item.title));
 
-    this.$watch(()=>this.inputText,(value)=>{
-      console.log('>App->watch:inputText=',value);
-      localStorage.setItem(LOCAL_KEY_INPUT_TEXT,JSON.stringify(value));
+    todoStopWatch = this.$watch(() => this.todos, (value) => {
+      console.log('> App -> watch: todos =', value);
+      localStorage.setItem(LOCAL_KEY_TODOS, JSON.stringify(value));
+    }, { deep: true });
+    this.$watch(() => this.inputText, (value) => {
+      console.log('> App -> watch: inputText =', value);
+      localStorage.setItem(LOCAL_KEY_INPUT_TEXT, JSON.stringify(value));
     });
-    },
-  unmounted(){
-      todoStopWath();
-    },
-  mounted(){
-     console.log('>App->mounted',{dom:this.$refs.domInput});
-    },
+  },
+  unmounted() {
+    todoStopWatch();
+  },
+  mounted() {
+    console.log('> App -> mounted', { dom: this.$refs.domInput });
+  },
   methods: {
-    onInputKeyUp() {
-      console.log('>App->onInputKeyUp:',this.todoText);
+    onInputEnterKeyUp() {
+      console.log('> App -> onInputEnterKeyUp:', this.todoText);
       this.todos.push(this.todoText);
-      this.inputText='';
+      this.inputText = '';
     },
-    onDeleteTodo(index){
-      console.log('>App->onDeleteTodo:index=',index);
-      this.todos.splice(index,1);
-
-    },
-  
-    
+    onDeleteTodo(index) {
+      console.log('> App -> onDeleteTodo: index =', index);
+      this.todos.splice(index, 1);
+    }
   }
-}; 
+};
 </script>
- 
-<style scoped></style>
