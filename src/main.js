083105aa7713f8b125a73af283b357a94ee13fa6
Igor Charuyskiy <haos77792@list.ru>
2023-05-18 18:43:14 +0300

    commit main.js lesson 29

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import './style.css';
 import AppComposition from './AppComposition.vue';
+import TodosPage from './components/TodosPage.vue';
+import IndexPage from './components/IndexPage.vue';
 
-createApp(AppComposition).mount('#app');
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            component: IndexPage
+        },
+        {
+            path: '/todos',
+            component: TodosPage
+        }
+    ],
+});
+
+createApp(AppComposition)
+    .use(router)
+    .mount('#app');
